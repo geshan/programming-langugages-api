8e3494629d3bc3bf28135b91e71f0ae1f4629211
Geshan Manandhar <geshan.manandhar@theiconic.com.au>
2021-01-31 22:29:00 +1100

    Setup express with ok message

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+app.get('/', (req, res) => {
+  res.json({'message': 'ok'});
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+});
