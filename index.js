3afd91cce530f7128755fed8e9651ee61966c552
Geshan Manandhar <geshan.manandhar@theiconic.com.au>
2021-02-01 22:29:52 +1100

    POST a new programming language endpoint

--- index.js
+++ index.js
@@ -17,6 +17,15 @@ app.get('/', (req, res) => {
 
 app.use('/programming-languages', programmingLanguagesRouter);
 
+/* Error handler middleware */
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  console.error(err.message, err.stack);
+  res.status(statusCode).json({'message': err.message});
+  
+  return;
+});
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 });
