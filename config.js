785eb17ba6887ef80c47fd73087d80427595bbdc
Geshan Manandhar <geshan.manandhar@theiconic.com.au>
2021-02-27 09:48:40 +1100

    New credentials

--- config.js
+++ config.js
@@ -3,9 +3,9 @@ const env = process.env;
 const config = {
   db: { /* don’t expose password or any sensitive info, done only for demo */
     host: env.DB_HOST || 'freedb.tech',
-    user: env.DB_USER || 'freedbtech_geshan-lr',
+    user: env.DB_USER || 'freedbtech_geshann',
     password: env.DB_PASSWORD || 'G2VjjQ5d47zyjqX',
-    database: env.DB_NAME || 'freedbtech_language',
+    database: env.DB_NAME || 'freedbtech_languagesabc',
   },
   listPerPage: env.LIST_PER_PAGE || 10,
 };
